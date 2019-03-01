69da58f213d78065070b433117c8aea3410b5854
Adam Roberts <skaidon.roberts@gmail.com>
2019-03-01 11:26:37 -0800

    Updates

---
+++ precache-manifest.66c6d2157aaa645d068b0a98dcc1b45c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee43125eb9e11b7d5ada",
+    "url": "/stanford//build/static/css/main.435b4d45.chunk.css"
+  },
+  {
+    "revision": "ee43125eb9e11b7d5ada",
+    "url": "/stanford//build/static/js/main.ee43125e.chunk.js"
+  },
+  {
+    "revision": "4cb1227d89ba9595e27e",
+    "url": "/stanford//build/static/js/runtime~main.4cb1227d.js"
+  },
+  {
+    "revision": "cfb487fa11f32b218627",
+    "url": "/stanford//build/static/js/2.cfb487fa.chunk.js"
+  },
+  {
+    "revision": "87f57611b0c3d41431c054525b69f1e2",
+    "url": "/stanford//build/index.html"
+  }
+];
