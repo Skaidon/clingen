bbd46f94fbb50285843b9b1ed1129b20d673f581
Adam Roberts <skaidon.roberts@gmail.com>
2019-03-01 11:26:51 -0800

    Updates

---
+++ precache-manifest.e6f7199a41b333a4731f3786d7529311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17ffdddb743a91c1ef75",
+    "url": "/stanford/build/static/css/main.435b4d45.chunk.css"
+  },
+  {
+    "revision": "17ffdddb743a91c1ef75",
+    "url": "/stanford/build/static/js/main.17ffdddb.chunk.js"
+  },
+  {
+    "revision": "b4539c06ab47ba47412f",
+    "url": "/stanford/build/static/js/runtime~main.b4539c06.js"
+  },
+  {
+    "revision": "cfb487fa11f32b218627",
+    "url": "/stanford/build/static/js/2.cfb487fa.chunk.js"
+  },
+  {
+    "revision": "936d086f3f27f54195ec3ce3c043dd06",
+    "url": "/stanford/build/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stanford//build/precache-manifest.66c6d2157aaa645d068b0a98dcc1b45c.js"
+  "/stanford/build/precache-manifest.e6f7199a41b333a4731f3786d7529311.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stanford//build/index.html", {
+workbox.routing.registerNavigationRoute("/stanford/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
