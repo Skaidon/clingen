c56fa2a84d7b307747daaf0530e561554a1fd8cc
Adam Roberts <skaidon.roberts@gmail.com>
2019-03-06 08:24:37 -0800

    Updates

---
+++ precache-manifest.c65c352a24acf12c548789086b063bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fca04cff984c0d0e36a",
+    "url": "/clingen/static/css/main.41c45e49.chunk.css"
+  },
+  {
+    "revision": "6fca04cff984c0d0e36a",
+    "url": "/clingen/static/js/main.6fca04cf.chunk.js"
+  },
+  {
+    "revision": "d356205d2dfae98f786e",
+    "url": "/clingen/static/js/runtime~main.d356205d.js"
+  },
+  {
+    "revision": "cbf16cc77d50566ef85a",
+    "url": "/clingen/static/css/2.f1b177c0.chunk.css"
+  },
+  {
+    "revision": "cbf16cc77d50566ef85a",
+    "url": "/clingen/static/js/2.cbf16cc7.chunk.js"
+  },
+  {
+    "revision": "e88fb9d6440947483ca6ee52f1d8a87d",
+    "url": "/clingen/index.html"
+  }
+];
