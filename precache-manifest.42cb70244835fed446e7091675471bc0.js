42de479146fef414e6f9025a960f9bb991cd6efd
Adam Roberts <skaidon.roberts@gmail.com>
2019-03-07 19:48:00 -0800

    Updates

--- precache-manifest.42cb70244835fed446e7091675471bc0.js
+++ precache-manifest.42cb70244835fed446e7091675471bc0.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "501df1e35b1719f64311",
+    "revision": "86c343d7c676ecc81ff4",
     "url": "/clingen/static/css/main.983156ec.chunk.css"
   },
   {
-    "revision": "501df1e35b1719f64311",
-    "url": "/clingen/static/js/main.ceae61d2.chunk.js"
+    "revision": "86c343d7c676ecc81ff4",
+    "url": "/clingen/static/js/main.f6b1a236.chunk.js"
   },
   {
     "revision": "d356205d2dfae98f786e",
     "url": "/clingen/static/js/runtime~main.ed3b996a.js"
   },
   {
-    "revision": "6c91b15cf07b23e8d1ae",
+    "revision": "17995a007befe7800acc",
     "url": "/clingen/static/css/2.f1b177c0.chunk.css"
   },
   {
-    "revision": "6c91b15cf07b23e8d1ae",
-    "url": "/clingen/static/js/2.abe66c0a.chunk.js"
+    "revision": "17995a007befe7800acc",
+    "url": "/clingen/static/js/2.274233b1.chunk.js"
   },
   {
-    "revision": "57442b0edade8f4a64e94081497bfaed",
+    "revision": "dabb7b8e65e387c99884c5cc69e73db5",
     "url": "/clingen/index.html"
   }
 ];
